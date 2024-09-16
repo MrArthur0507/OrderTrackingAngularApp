35fdad0c12ea2010aa6ab7a0452cd3e31a57df62
MrArthur0507 <mrarthur0507@gmail.com>
2024-09-16 14:59:47 +0300

    working login and logout with prototype navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { OAuthService, AuthConfig, provideOAuthClient } from 'angular-oauth2-oidc';
-
+import { OAuthService, AuthConfig, provideOAuthClient, JwksValidationHandler } from 'angular-oauth2-oidc';
+import { CommonModule } from '@angular/common';
+import { map, Observable, of } from 'rxjs';
 const authConfig: AuthConfig = {
   issuer: 'http://localhost:5000',
   clientId: 'angular',
@@ -10,7 +11,7 @@ const authConfig: AuthConfig = {
   scope: 'openid profile',
   responseType: 'code',
   showDebugInformation: true,
-  sessionChecksEnabled: true,
+  requestAccessToken: true
 };
 
 
@@ -18,37 +19,44 @@ const authConfig: AuthConfig = {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit{
-  constructor(private oauthService: OAuthService) {}
+  constructor(private oauthService: OAuthService) {
+    this.initAuth();
+  }
   ngOnInit(): void {
-    this.configureOAuth();
   }
   title = 'order-tracking-angular-app';
 
-  configureOAuth() {
+  initAuth() {
     this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
     this.oauthService.loadDiscoveryDocumentAndTryLogin();
-
-    if (!this.oauthService.hasValidAccessToken()) {
-      this.oauthService.initCodeFlow();
-    }
+  }
+  isLoggedIn(): Observable<boolean> {
+    return of(this.oauthService.hasValidAccessToken()).pipe(
+      map(hasValidToken => !!hasValidToken)
+    );
   }
   login() {
-    this.oauthService.initCodeFlow(); 
+    this.oauthService.initCodeFlow();
   }
 
   logout() {
     this.oauthService.logOut();
   }
 
-  isLoggedIn() {
-    return this.oauthService.hasValidAccessToken();
-  }
 
+  getAccessToken(): boolean {
+    const accessToken = this.oauthService.getAccessToken();
+    if (accessToken != null) {
+      return true;
+    }
+    return false;
+  }
 
 
 }
