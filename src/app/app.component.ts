d1a9600663281fa13b5d50ede793e1f286bf01e6
MrArthur0507 <mrarthur0507@gmail.com>
2024-09-16 14:01:00 +0300

    added angular-oauth2-oidc library and basic logic for sso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { OAuthService, AuthConfig, provideOAuthClient } from 'angular-oauth2-oidc';
+
+const authConfig: AuthConfig = {
+  issuer: 'http://localhost:5000',
+  clientId: 'angular',
+  redirectUri: window.location.origin,
+  scope: 'openid profile',
+  responseType: 'code',
+  showDebugInformation: true,
+  sessionChecksEnabled: true,
+};
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private oauthService: OAuthService) {}
+  ngOnInit(): void {
+    this.configureOAuth();
+  }
   title = 'order-tracking-angular-app';
+
+  configureOAuth() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+
+    if (!this.oauthService.hasValidAccessToken()) {
+      this.oauthService.initCodeFlow();
+    }
+  }
+  login() {
+    this.oauthService.initCodeFlow(); 
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+
+  isLoggedIn() {
+    return this.oauthService.hasValidAccessToken();
+  }
+
+
+
 }
