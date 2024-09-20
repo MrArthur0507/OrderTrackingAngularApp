def4fc9427ba3bb19b4973a0c7a51dc033f22e5b
MrArthur0507 <mrarthur0507@gmail.com>
2024-09-20 15:51:38 +0300

    separated auth logic in auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { OAuthService, AuthConfig, provideOAuthClient, JwksValidationHandler } from 'angular-oauth2-oidc';
 import { CommonModule } from '@angular/common';
 import { map, Observable, of } from 'rxjs';
 import { CatalogItemsComponent } from './catalog-items/catalog-items.component';
-const authConfig: AuthConfig = {
-  issuer: 'http://localhost:5000',
-  clientId: 'angular',
-  redirectUri: window.location.origin,
-  scope: 'openid profile',
-  responseType: 'code',
-  showDebugInformation: true,
-  requestAccessToken: true
-};
-
+import { AuthService } from './auth.service';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CommonModule, CatalogItemsComponent],
+  providers: [AuthService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit{
-  constructor(private oauthService: OAuthService) {
-    this.initAuth();
-  }
-  ngOnInit(): void {
-  }
   title = 'order-tracking-angular-app';
 
-  initAuth() {
-    this.oauthService.configure(authConfig);
-    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
-    this.oauthService.loadDiscoveryDocumentAndTryLogin();
-  }
+  constructor(private authService: AuthService) {} 
+
+  ngOnInit(): void {}
+
   isLoggedIn(): Observable<boolean> {
-    return of(this.oauthService.hasValidAccessToken()).pipe(
-      map(hasValidToken => !!hasValidToken)
-    );
-  }
-  login() {
-    this.oauthService.initCodeFlow();
+    return this.authService.isLoggedIn(); 
   }
 
-  logout() {
-    this.oauthService.logOut();
+  login(): void {
+    this.authService.login(); 
   }
 
+  logout(): void {
+    this.authService.logout(); 
+  }
 
   getAccessToken(): boolean {
-    const accessToken = this.oauthService.getAccessToken();
-    if (accessToken != null) {
-      return true;
-    }
-    return false;
+    return !!this.authService.getAccessToken(); 
   }
 
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
+import { AuthService } from './auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideOAuthClient()]
